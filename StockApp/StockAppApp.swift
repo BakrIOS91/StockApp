3d9af166b593bbbaaea19887d725951179fecbde
Bakr Mohamed <>
2026-03-24 15:21:12 +0200

    Create App

---
+++ StockApp/StockAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  StockAppApp.swift
+//  StockApp
+//
+//  Created by Bakr Mohamed on 24/03/2026.
+//
+
+import SwiftUI
+
+@main
+struct StockAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
